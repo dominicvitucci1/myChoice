cb5315336e9dc156bf0126b835160090a3db9818
dominicvitucci1 <Dominic@Dominic-Vituccis-MacBook.local>
2017-12-09 11:11:34 -0600

    Build 2 Updates

---
+++ android/app/src/main/java/com/dominicvitucci/mychoicecontraception/MainApplication.java
@@ -0,0 +1,56 @@
+package com.dominicvitucci.mychoicecontraception;
+
+import android.app.Application;
+
+import com.facebook.react.ReactApplication;
+import net.no_mad.tts.TextToSpeechPackage;
+import com.audioStreaming.ReactNativeAudioStreamingPackage;
+import fm.indiecast.rnaudiostreamer.RNAudioStreamerPackage;
+import com.avishayil.rnrestart.ReactNativeRestartPackage;
+import com.dylanvann.fastimage.FastImageViewPackage;
+import com.babisoft.ReactNativeLocalization.ReactNativeLocalizationPackage;
+import com.oblador.vectoricons.VectorIconsPackage;
+import com.AlexanderZaytsev.RNI18n.RNI18nPackage;
+import com.facebook.react.ReactNativeHost;
+import com.facebook.react.ReactPackage;
+import com.facebook.react.shell.MainReactPackage;
+import com.facebook.soloader.SoLoader;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class MainApplication extends Application implements ReactApplication {
+
+  private final ReactNativeHost mReactNativeHost = new ReactNativeHost(this) {
+    @Override
+    public boolean getUseDeveloperSupport() {
+      return BuildConfig.DEBUG;
+    }
+
+    @Override
+    protected List<ReactPackage> getPackages() {
+      return Arrays.<ReactPackage>asList(
+          new MainReactPackage(),
+            new TextToSpeechPackage(),
+            new ReactNativeAudioStreamingPackage(),
+            new RNAudioStreamerPackage(),
+            new ReactNativeRestartPackage(),
+            new FastImageViewPackage(),
+            new ReactNativeLocalizationPackage(),
+            new VectorIconsPackage(),
+            new RNI18nPackage()
+      );
+    }
+  };
+
+  @Override
+  public ReactNativeHost getReactNativeHost() {
+    return mReactNativeHost;
+  }
+
+  @Override
+  public void onCreate() {
+    super.onCreate();
+    SoLoader.init(this, /* native exopackage */ false);
+  }
+}
